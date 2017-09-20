5706a25429329a5fba8a9accbacb9980abd7d65f
Eric Rovtar <eric.rovtar@me.com>
2017-09-19 20:14:20 -0400

    Add camera function

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera } from 'ionic-native';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+	selector: 'page-about',
+	templateUrl: 'about.html'
 })
 export class AboutPage {
+	imageUrl;
+	constructor(public navCtrl: NavController) {
 
-  constructor(public navCtrl: NavController) {
+	}
 
-  }
-
+	takePhoto() {
+		Camera.getPicture().then((imageData) => {
+			this.imageUrl = imageData
+		}, (err) => {
+			console.log(err);
+		});
+	}
 }
