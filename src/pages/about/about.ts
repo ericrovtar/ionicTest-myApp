3688a63b43f2c36e68516c6c747fc46f81942cf6
Eric Rovtar <eric.rovtar@me.com>
2017-09-21 22:03:20 -0400

    add alternative photo methods

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -8,6 +8,7 @@ import { Camera, CameraOptions } from 'ionic-native';
 })
 export class AboutPage {
 	imageUrl;
+	public base64Image: string;
 	error: string;
 
 	constructor(public navCtrl: NavController) {
@@ -19,6 +20,21 @@ export class AboutPage {
 			this.imageUrl = imageData;
 		}, (err) => {
 			this.error = err;
+			console.log(err);
+		});
+	}
+
+	takePicture(){
+		Camera.getPicture({
+			destinationType: Camera.DestinationType.DATA_URL,
+			targetWidth: 1000,
+			targetHeight: 1000
+		}).then((imageData) => {
+		  // imageData is a base64 encoded string
+			this.base64Image = "data:image/jpeg;base64," + imageData;
+
+			// this.captureDataUrl = this.sanitizer.bypassSecurityTrustUrl('data:image/jpeg;base64,' + imageData)
+		}, (err) => {
 			console.log(err);
 		});
 	}
