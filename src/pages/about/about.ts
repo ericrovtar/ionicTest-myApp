d103a1ca764df46de04e2b0f881504029e1595e4
Eric Rovtar <eric.rovtar@me.com>
2017-09-21 23:05:08 -0400

    test new photo taking

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,47 +1,68 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Camera } from 'ionic-native';
+import { Camera, CameraOptions } from 'ionic-native';
 
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser'
+// import { DomSanitizer, SafeUrl } from '@angular/platform-browser'
 
 @Component({
 	selector: 'page-about',
 	templateUrl: 'about.html'
 })
 export class AboutPage {
-	imageUrl;
-	public base64Image: string;
-	captureDataUrl: SafeUrl;
-	error: string;
+	// imageUrl;
+	// public base64Image: string;
+	// captureDataUrl: SafeUrl;
+	// error: string;
 
 	constructor(
 		public navCtrl: NavController,
-		private sanitizer: DomSanitizer) {
+		// private sanitizer: DomSanitizer
+	) {
 
 	}
 
-	takePhoto() {
-		Camera.getPicture().then((imageData) => {
-			this.imageUrl = imageData;
-		}, (err) => {
-			this.error = err;
-			console.log(err);
-		});
-	}
+	captureDataUrl: string;
 
-	takePicture(){
-		Camera.getPicture({
+	capture() {
+		//setup camera options
+		const cameraOptions: CameraOptions = {
+			quality: 50,
 			destinationType: Camera.DestinationType.DATA_URL,
-			targetWidth: 1000,
-			targetHeight: 1000
-		}).then((imageData) => {
-		  	// imageData is a base64 encoded string
-			this.base64Image = "data:image/jpeg;base64," + imageData;
+			encodingType: Camera.EncodingType.JPEG,
+			mediaType: Camera.MediaType.PICTURE,
+		};
 
-			this.captureDataUrl = this.sanitizer.bypassSecurityTrustUrl('data:image/jpeg;base64,' + imageData);
+		Camera.getPicture(cameraOptions).then((imageData) => {
+			// imageData is either a base64 encoded string or a file URI
+			// If it's base64:
+			this.captureDataUrl = 'data:image/jpeg;base64,' + imageData;
 		}, (err) => {
-			this.error = err;			
-			console.log(err);
+			// Handle error
 		});
 	}
+
+	// takePhoto() {
+	// 	Camera.getPicture().then((imageData) => {
+	// 		this.imageUrl = imageData;
+	// 	}, (err) => {
+	// 		this.error = err;
+	// 		console.log(err);
+	// 	});
+	// }
+
+	// takePicture(){
+	// 	Camera.getPicture({
+	// 		destinationType: Camera.DestinationType.DATA_URL,
+	// 		targetWidth: 1000,
+	// 		targetHeight: 1000
+	// 	}).then((imageData) => {
+	// 	  	// imageData is a base64 encoded string
+	// 		this.base64Image = "data:image/jpeg;base64," + imageData;
+
+	// 		this.captureDataUrl = this.sanitizer.bypassSecurityTrustUrl('data:image/jpeg;base64,' + imageData);
+	// 	}, (err) => {
+	// 		this.error = err;			
+	// 		console.log(err);
+	// 	});
+	// }
 }
